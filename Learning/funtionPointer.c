c526abb8319f7e23195465ba6067d16cb5471c4c
Moses <jangmoses46@gmail.com>
2025-11-25 20:04:42 +0900

    funtionPointer

--- Learning/funtionPointer.c
+++ Learning/funtionPointer.c
@@ -1,18 +1,80 @@
 #include <stdio.h>
+#include <string.h>
 
-      int add(int a, int b);
+      int add(int x, int y);
+      int sub(int x, int y);
+      int mult(int x, int y);
 
-      int main(){
+ int main(){
+      char userInput[100];
+      int x;
+      int y;
 
-            int (*pointer)(int, int) = add;
-            int result = pointer(4,5);
+      printf("Choose your operator: "
+            "\n1.addition"
+            "\n2.subtraction"
+            "\n3.mutliplication"
+            "\n Choice: ");
+            scanf("%s", userInput);
+
+            int (*funPrt)(int, int);
+
+      if(strcmp(userInput, "add") == 0 ){
+
+            funPrt = add;
+
+            printf("\nEnter firstNum: ");
+            scanf("%d", &x);
+            printf("\n Enter SecondNum: ");
+            scanf("%d", &y);
+
+       printf("result: %d",funPrt(x, y));
+      }
+      else if(strcmp(userInput,"sub") == 0){
+
             
-            printf("%d", result);
+            funPrt = sub;
+            
+            printf("\nEnter firstNum: ");
+            scanf("%d", &x);
+            printf("\n Enter SecondNum: ");
+            scanf("%d", &y);
 
-
-            return 0;
+            printf("result: %d",funPrt(x, y));
       }
-      int add(int a, int b){
+            else if(strcmp(userInput,"mult") == 0){
 
-            return a+b;
+            funPrt = mult;
+            
+            printf("\nEnter firstNum: ");
+            scanf("%d", &x);
+            printf("\n Enter SecondNum: ");
+            scanf("%d", &y);
+            
+            printf("result: %d",funPrt(x, y));
       }
+
+      
+
+      return 0;
+
+ }
+
+ int add(int x, int y){
+
+      return x+y;
+ }
+
+  int sub(int x, int y){
+
+      return x-y;
+ }
+
+ int mult(int x , int y){
+
+      return x*y;
+ }
+
+ // what i learned:
+ // an array is a pointer so you dont need it address when doing stuff like scanf
+ // strcmp is use to compare 2 strings
