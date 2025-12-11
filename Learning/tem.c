5cc534d4c2a9f03dac0b4261c31b2391a1b2d9f6
Moses <jangmoses46@gmail.com>
2025-12-11 20:16:27 +0900

    reallocation of memory

--- Learning/tem.c
+++ Learning/tem.c
@@ -2,9 +2,23 @@
 #include <stdlib.h>
       int main(){
             int *Ptr1, *Ptr2;
-            Ptr1 = malloc(2);
-
+            int size = 2*sizeof(int);
+            Ptr1 = malloc(size);
             printf("%p", Ptr1);
+
+            size = 4*sizeof(*Ptr1);
+            Ptr2 = realloc(Ptr1, size);
+            printf("\n%p", Ptr2);
+
+            if(Ptr2 == NULL){
+                  printf("Unsuccessful");
+
+            }
+            else{
+                  printf("\nYEAP!!!");
+            }
+            free(Ptr2);
+            Ptr2 = NULL;
 
             return 0;
       }
