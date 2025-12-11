82a7e138e7abdbe5809b7aec2b452056a7ccff79
Moses <jangmoses46@gmail.com>
2025-12-11 19:22:30 +0900

    allocation time

--- Learning/tem.c
+++ Learning/tem.c
@@ -1,7 +1,10 @@
 #include <stdio.h>
+#include <stdlib.h>
       int main(){
             int *Ptr1, *Ptr2;
+            Ptr1 = malloc(2);
 
+            printf("%p", Ptr1);
 
             return 0;
       }
