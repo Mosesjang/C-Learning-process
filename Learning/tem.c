5f65ae0ffaa10820b82a5bf3b9ce0274f2de3d11
Moses <jangmoses46@gmail.com>
2025-12-11 19:12:57 +0900

    ghhjhjhg

--- Learning/tem.c
+++ Learning/tem.c
@@ -1,15 +1,7 @@
 #include <stdio.h>
-#include <stdlib.h>
-     int main(){
+      int main(){
+            int *Ptr1, *Ptr2;
 
-          int* Ptr = calloc(4,sizeof(Ptr));
-          *Ptr = 67;
-          Ptr[1] = 69;
 
-               printf("\nYour number is: %d,", *Ptr);
-               printf("\nYour number is: %d,", Ptr[1]);
-
-          free(Ptr);
-
-          return 0;
-     }
+            return 0;
+      }
