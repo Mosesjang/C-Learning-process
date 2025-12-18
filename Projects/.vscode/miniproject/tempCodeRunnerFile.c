4e90ad8618f02088cded063363a894c7b35cece5
Moses <jangmoses46@gmail.com>
2025-12-18 15:08:17 +0900

    josy

--- Projects/.vscode/miniproject/tempCodeRunnerFile.c
+++ Projects/.vscode/miniproject/tempCodeRunnerFile.c
@@ -1,1 +1,16 @@
- ans
+#include <stdio.h>
+
+int  addition(int randNum[]);
+int randNum[100] = {10,20};
+ 
+int main(){
+
+      printf("Sum = %d", addition(randNum));
+
+      return 0;
+}
+
+int addition(int ranNum[]){
+
+      return randNum[0] + randNum[1];
+}
