fcb6eae0e2bbb3152aba990b0e3730b0a993665e
Moses <jangmoses46@gmail.com>
2025-12-21 14:44:03 +0900

    jjojjoj

--- Repetition/REPS.c
+++ Repetition/REPS.c
@@ -1,7 +1,9 @@
 #include <stdio.h>
 #include <string.h>
 
-int nameInputFunc(char (*ptr)[50], int count);
+void nameInputFunc(char (*ptr)[50], int count);
+void printName(char (*ptr)[50], int count);
+void searchName(char (*ptr)[50], int count);
 
 int main(){
       int count = 5;
@@ -9,11 +11,14 @@ int main(){
       
 
       nameInputFunc(names, count);
+      printName(names, count);
+      searchName(names, count);
+
 
       return 0;
 }
 
-int nameInputFunc(char (*ptr)[50], int count){
+void nameInputFunc(char (*ptr)[50], int count){
 
       printf("\n===== Name List Manager =====\n");
       printf("Enter 5 names\n");
@@ -26,4 +31,31 @@ int nameInputFunc(char (*ptr)[50], int count){
       }
 
       return 0;
+}
+
+void printName(char (*ptr)[50], int count){
+
+      for(int x = 0; x < count; x++){
+             printf("\nName %d: %s", x + 1, ptr[x]);
+      }
+     
+}
+
+void searchName(char (*ptr)[50], int count){
+      char choice[50];
+      int found = 0;
+      printf("\nSearch Name: ");
+      scanf("%s", choice);
+      for(int x = 0; x < count; x++){
+           int result = strcmp(ptr[x], choice);
+           if(result == 0){
+            printf("Found at position %d\n", x + 1);
+            found = 1;
+            break;
+           }
+      }
+
+      if(found == 0){
+            printf("Name not found");
+      }
 }
