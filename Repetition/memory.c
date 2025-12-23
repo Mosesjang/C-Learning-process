01376bbb8f6938b7dcc8becf4b41c58b3a0886b9
Moses <jangmoses46@gmail.com>
2025-12-23 15:05:18 +0900

    hff

--- Repetition/memory.c
+++ Repetition/memory.c
@@ -9,5 +9,9 @@ int main(){
       scanf("%d", &count);
 
       char(*name)[50] = malloc(count * sizeof(*name));
+      if(name == NULL){
+            printf("memory allocation failed");
+            return 1;
+      }
       return 0;
 }
