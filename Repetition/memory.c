6ad55a608860625f21d735704300092410f10ce2
Moses <jangmoses46@gmail.com>
2025-12-30 18:33:27 +0900

    hgihi

--- Repetition/memory.c
+++ Repetition/memory.c
@@ -1,6 +1,10 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
+void getName(char (*name)[50], int count);
+void printName(char (*name)[50], int count);
+int searchName(char (*name)[50], int count);
 
 int main(){
       int count;
@@ -10,8 +14,55 @@ int main(){
 
       char(*name)[50] = malloc(count * sizeof(*name));
       if(name == NULL){
-            printf("memory allocation failed");
+            printf("memory allocation failed\n");
             return 1;
       }
+      
+      getName(name, count);
+      printName(name, count);
+      int pos = searchName(name, count);
+
+      if(pos != -1){
+            printf("found at %d", pos);
+      }
+      else{
+            printf("name does not exist");
+      }
+
+      free(name);
       return 0;
 }
+
+void getName(char (*name)[50], int count){
+      printf("Enter Names:\n");
+      for(int x = 0; x < count; x++){
+            printf("Name %d:", x + 1);
+            scanf("%s", name[x]);
+      }
+
+}
+
+void printName(char(*name)[50], int count){
+      printf("\n===== List of names =====\n");
+      for(int x = 0; x < count; x++){
+            printf("Name %d: %s\n", x + 1, name[x]);
+      }
+}
+
+int searchName(char (*name)[50], int count){
+      char choice[50];
+      printf("\nSearch Name: ");
+      scanf("%s", choice);
+
+      for(int x = 0; x < count; x++){
+            int result = strcmp(name [x], choice);
+            if(result == 0){
+                  return x + 1;
+            }
+      }
+
+      return -1;
+}
+
+// things learned: 1. allocate 2d array with pointer char (*ptr)[fix number] = malloc(count * sizeof(*name));
+// Number 2: you can use return multiple times, plus you can create a variable that stores function return value.
