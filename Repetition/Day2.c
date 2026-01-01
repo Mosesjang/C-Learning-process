00637cb7ee0df2d1b12e40347b37f1a3d3f0af8e
Moses <jangmoses46@gmail.com>
2026-01-01 11:48:50 +0900

    done with day 2 speed run

--- Repetition/Day2.c
+++ Repetition/Day2.c
@@ -1,1 +1,38 @@
+#include <stdio.h>
+#include <string.h>
 
+struct Classroom{
+      int id;
+      int age;
+      char name[50];
+};
+
+int main(){
+
+      int count = 0;
+
+      printf("How many students? ");
+      scanf("%d", &count);
+
+      struct Classroom students[count];
+
+      for(int x = 0; x < count; x++){
+            printf("Enter student %d name: ", x+1);
+            scanf("%s", students[x].name);
+            printf("Enter student %d age: ", x+1);
+            scanf("%d", &students[x].age);
+      }
+
+      printf("\n----- Student List -----\n");
+
+      for(int x = 0; x < count; x++){
+
+            students[x].id = x + 1;
+
+            printf("ID: %d | Name: %s | Age: %d\n", 
+                  students[x].id, 
+                  students[x].name, 
+                  students[x].age);
+      }
+      return 0;
+}
