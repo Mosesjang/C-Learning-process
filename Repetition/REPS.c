59cc3700c20b93262cca9743e957a577919ac318
Moses <jangmoses46@gmail.com>
2026-01-11 23:33:20 +0900

    added a search id feature

--- Repetition/REPS.c
+++ Repetition/REPS.c
@@ -8,6 +8,7 @@ typedef struct {
 
 void getName(Classroom *s, int count);
 void printName(Classroom *s, int count);
+void searchID(Classroom *s, int count);
 
 int main(){
       int choice;
@@ -19,6 +20,7 @@ int main(){
 
       getName(students, choice);
       printName(students, choice);
+      searchID(students, choice);
 
       return 0;
 }
@@ -39,6 +41,22 @@ void printName(Classroom *s, int count){
       for(int x = 0; x < count; x++){
             printf("ID: %d | Name: %s | Age: %d\n", 
                   s[x].id, s[x].name, s[x].age);
+      }
+}
+
+void searchID(Classroom *s, int count){
+      int idInput;
+      printf("\nSearch ID : ");
+      scanf("%d", &idInput);
+
+      for(int x = 0; x < count; x++){
+            int temId = s[x].id;
+            if(idInput == temId ){
+                  printf("ID %d belongs to: \n", temId);
+                  printf("ID: %d | Name: %s | Age: %d\n", 
+                  s[x].id, s[x].name, s[x].age);
+                  break;
+            }
       }
 }
 
