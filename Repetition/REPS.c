4005fe0dd508bad312d7ab1a022b437fdae985ba
Moses <jangmoses46@gmail.com>
2026-01-12 10:30:01 +0900

    done dada

--- Repetition/REPS.c
+++ Repetition/REPS.c
@@ -1,1 +1,13 @@
+#include <stdio.h>
 
+int main(){
+
+      int x[4] = {3,6,5,4};
+
+      printf("%d\n", x[0]);
+      for(int i = 0; i < 4; i++){
+            printf("\n%d", *(x + i));
+      }
+
+      return 0;
+}
