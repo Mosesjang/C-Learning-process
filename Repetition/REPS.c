3f79a37d05f4afcbac2d6588708281586ab5932c
Moses <jangmoses46@gmail.com>
2026-01-12 09:53:38 +0900

    just learnt that if your are printing an adress you must use %p

--- Repetition/REPS.c
+++ Repetition/REPS.c
@@ -1,1 +1,14 @@
+#include <stdio.h>
 
+int main(){
+      int x = 10;
+
+      int *p = &x;
+
+      printf("x value: %d\n", x);
+      printf("x address: %p\n", (void*)&x);
+      printf("p value(address): %p\n", (void*)p);
+      printf("value pointed by p: %d\n", *p);
+
+      return 0;
+}
