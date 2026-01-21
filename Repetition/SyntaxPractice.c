b3459ffed5120b787ccca1d7196b261fa8582340
Moses <jangmoses46@gmail.com>
2026-01-21 20:55:15 +0900

    rubbish but works

--- Repetition/SyntaxPractice.c
+++ Repetition/SyntaxPractice.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 typedef struct{
       int id;
@@ -7,28 +8,40 @@ typedef struct{
       char name[50];
 }Sensor;
 
-void getSenInput(Sensor *s, int amount);
+void getSenInput(Sensor *s, int amount, int plus);
 void printSenData(Sensor *s, int amount);
 
 int main(){
-      int amount;
+      int amount = 0;
       printf("How many sensors? ");
       scanf("%d", &amount);
 
       Sensor *s = malloc(amount * sizeof(Sensor));
       if(!s) return 1;
 
-      getSenInput(s, amount);
+      getSenInput(s, 0, amount);
       printSenData(s, amount);
+      // Ask for more Sensors
+      int more;
+      printf("How many more sensors? ");
+      scanf("%d", &more);
+      int newCount = amount + more;
+      // Create space for more sensors
+      Sensor *t = realloc(s, amount * sizeof(Sensor));
+      s = t;
+      getSenInput(s, amount, newCount);
+      printSenData(s, newCount);
+
+
 
       free(s);
 
       return 0;
 }
 
-void getSenInput(Sensor *s, int amount){
+void getSenInput(Sensor *s, int amount, int plus){
       printf("\n===== Sensor Info =====\n");
-      for(int x = 0; x < amount; x++){
+      for(int x = amount; x < plus; x++){
             printf("Enter Sensor %d info:\n", x+1);
             printf("Name: ");
             scanf("%s", s[x].name);
@@ -39,9 +52,9 @@ void getSenInput(Sensor *s, int amount){
       }
 }
 
-void printSenData(Sensor *s, int amount){
+void printSenData(Sensor *s, int count){
       printf("\n----- Display Sensors -----\n");
-      for(int x = 0; x < amount; x++){
+      for(int x = 0; x < count; x++){
             printf("%d. ID: %d | Name: %s | Value: %.2f\n",
                   x + 1, s[x].id, s[x].name, s[x].Value);
       }
