e6198eb10be6e51032ea84cdf2976549c4cdf903
Moses <jangmoses46@gmail.com>
2026-02-08 20:48:08 +0900

    just done my first function pointer code

--- Repetition/SyntaxPractice.c
+++ Repetition/SyntaxPractice.c
@@ -1,1 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
 
+int add(int a, int b){ return a + b;};
+int sub(int a, int b){ return a - b;};
+
+int main(){
+
+      int (*f)(int, int);
+
+      f = add;
+
+      printf("%d", f(5, 10));
+
+      f = sub;
+
+      printf("%d\n", f(5, 10));
+
+
+      return 0;
+}
