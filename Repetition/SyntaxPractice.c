232190b9350113373ffcda36ac8c47cf4706ee11
Moses <jangmoses46@gmail.com>
2026-02-22 22:58:36 +0900

    done with less help

--- Repetition/SyntaxPractice.c
+++ Repetition/SyntaxPractice.c
@@ -9,9 +9,47 @@ typedef struct{
 void getStudentInpt(Student *s, int count);
 
 int main(){
+
+      // get number of students
       int studentAmt;
       printf("How many student? ");
       scanf("%d", &studentAmt);
+
+      //creat a struct array for each student
+       Student s[studentAmt];
+
+       // get student input
+       getStudentInpt(s, studentAmt);
+       
+       // Creat a file
+       FILE *fptr;
+       fptr = fopen("MyName.txt", "w");
+       if(!fptr){
+            printf("Unable to open file");
+            return 1;
+       }
+      for(int x = 0; x < studentAmt; x++){
+
+             fprintf(fptr, "Name: %s\nAge: %d\nScore: %.2f",s[x].name, s[x].age, s[x].score);
+       }
+       // close file
+       fclose(fptr);
+
+       // Write student data in file
+       char myString[100];
+       fptr = fopen("MyName.txt", "r");
+       if(!fptr){
+            printf("Unable to open file");
+            return 1;
+       }
+
+       // print file info in terminal
+       while(fgets(myString, 100, fptr)){
+            printf("%s", myString);
+       }
+
+       
+
 
 
 
@@ -22,12 +60,12 @@ void getStudentInpt(Student *s, int count){
 
       printf("\n===== Student Info Manager =====\n");
       for(int x = 0; x < count; x++){
-            printf("Enter student %d: ", x + 1);
+            printf("Enter student %d:\n", x + 1);
             printf("Name: ");
             scanf("%s", s[x].name);
             printf("Age: ");
             scanf("%d", &s[x].age);
-            printf("ID: ");
+            printf("Score: ");
             scanf("%f", &s[x].score);
       }
 }
