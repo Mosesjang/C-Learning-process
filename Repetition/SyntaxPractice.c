fadcb5465713168410596647d1b953d4848df69f
Moses <jangmoses46@gmail.com>
2026-02-22 21:49:14 +0900

    done

--- Repetition/SyntaxPractice.c
+++ Repetition/SyntaxPractice.c
@@ -1,1 +1,24 @@
 #include <stdio.h>
+
+
+
+int main(){
+
+      // Create File
+      FILE *fptr;
+      fptr = fopen("MyName.txt", "w");
+      
+      //write something in myfile
+
+      fprintf(fptr, "Hello World");
+      // close file
+      close(fptr);
+
+      // Read file 
+      char myString[100];
+      fptr = fopen("MyName.txt", "r");  
+      
+
+
+      return 0;
+}
