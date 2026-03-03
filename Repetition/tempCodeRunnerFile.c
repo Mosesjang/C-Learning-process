01319677c9aa5fdb59a216bbd1b55be1f123d623
Moses <jangmoses46@gmail.com>
2026-03-04 06:19:56 +0900

    done with files for now

--- Repetition/tempCodeRunnerFile.c
+++ Repetition/tempCodeRunnerFile.c
@@ -1,1 +1,13 @@
 #include <stdio.h>
+
+int main(){
+
+      FILE *ptr;
+      ptr = fopen("HelloWorld.txt", "w");
+      fprintf(ptr,"hello");
+
+      //close
+      fclose(ptr);
+
+      return 0;
+}
