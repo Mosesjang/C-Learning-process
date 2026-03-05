e9e33a121e58caf8e09e509436265092cf1e6189
Moses <jangmoses46@gmail.com>
2026-03-06 07:15:26 +0900

    done with day 2

--- Repetition/tempCodeRunnerFile.c
+++ Repetition/tempCodeRunnerFile.c
@@ -1,1 +1,17 @@
 #include <stdio.h>
+
+int main(){
+      unsigned char reg = 0b00101001;
+
+      for(int x = 0; x < 8; x++){
+            if(reg &(1 << x)){
+                  printf("Bit %c is ON", x);
+            }
+            else{
+                  printf("Bit %c is OFF", x);
+            }
+
+      }
+
+      return 0;
+}
