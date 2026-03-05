5e7c954bbd1bf1a9b66c8d24382a4dacf27ba5a3
Moses <jangmoses46@gmail.com>
2026-03-06 07:15:43 +0900

    done

--- Repetition/tempCodeRunnerFile.c
+++ Repetition/tempCodeRunnerFile.c
@@ -3,15 +3,30 @@
 int main(){
       unsigned char reg = 0b00101001;
 
+
+      //Toggling a bit 
+      reg ^= (1 << 3);
+
+      //Turning OFF a bit
+      reg &= ~(1 << 0);
+      reg &= ~(1 << 5);
+      
+
+      //Turning ON a bit
+      //reg |= (1 << 1);
+
+      // Checking if a bit is ON
       for(int x = 0; x < 8; x++){
-            if(reg &(1 << x)){
-                  printf("Bit %c is ON", x);
+            if(reg &=(1 << x)){
+                  printf("Bit %d is ON\n", x);
             }
             else{
-                  printf("Bit %c is OFF", x);
+                  printf("Bit %d is OFF\n", x);
             }
 
       }
+
+
 
       return 0;
 }
