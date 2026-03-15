ef359e9c1b36fe110358978ad926fc96e8d57f41
Moses <jangmoses46@gmail.com>
2026-03-15 22:51:01 +0900

    done

--- Repetition/tempCodeRunnerFile.c
+++ Repetition/tempCodeRunnerFile.c
@@ -5,22 +5,44 @@
 #define MOTOR_ENABLE (1 << 1)
 #define SENSOR_ENABLE (1 << 2)
 
+void enable(uint8_t *reg, uint8_t mask);
+void disable(uint8_t *reg, uint8_t mask);
+void isEnable(uint8_t *reg, uint8_t mask);
+
 int main(){
 
       uint8_t control = 0;
       
-      // Enable LED
-      control |= LED_ENABLE; 
+      // Enable LED & Motor
+      enable(&control, LED_ENABLE);
+      enable(&control, MOTOR_ENABLE);
       printf("%d\n", control);
 
-
-      // Enable sensor
-      control |= SENSOR_ENABLE;
-      printf("%d\n", control);
-
-      //Disable LED
-      control &= ~LED_ENABLE;
+      // Disable Motor
+      disable(&control, MOTOR_ENABLE);
       printf("%d\n", control);
       
+
+      //Checking led
+      isEnable(&control, LED_ENABLE);
+
+      
       return 0;
+}
+
+void enable(uint8_t *reg, uint8_t mask){
+      *reg |= mask;
+}
+
+void disable(uint8_t *reg, uint8_t mask){
+      *reg &= ~ mask;
+}
+
+void isEnable(uint8_t *reg, uint8_t mask){
+      if(*reg & mask){
+            printf("LED ON\n");
+      }
+      else{
+            printf("LED OFF\n");
+      }
 }
