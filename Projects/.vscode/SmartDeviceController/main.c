b045a2354b3ff95bc1b96837761335810ba63911
Moses <jangmoses46@gmail.com>
2026-03-20 06:34:31 +0900

    day 2 of my Project

--- Projects/.vscode/SmartDeviceController/main.c
+++ Projects/.vscode/SmartDeviceController/main.c
@@ -2,50 +2,72 @@
 #include <stdint.h>
 
 typedef struct{
-      char device[50];
+      char deviceName[50];
       uint8_t deviceState;
 }DeviceInfo;
 
 int connectDevices(DeviceInfo *s, int count);
-void addSmartDevice(int choice);
+int addSmartDevice(DeviceInfo *s, int choice, int deviceType, const char *one, const char *two, const char *three);
 
 int main (){
       //declare struct
-      DeviceInfo *s;
-
+      DeviceInfo s[10];
+      // Variables
       int choice;
-      printf("\n1. Add Smart Device "
+      int deviceType;
+      //constants
+      const char one[] = "LIGHT";
+      const char two[] = "MOTOR";
+      const char three[] = "SENSOR";
+
+      //main code start here
+      printf("\n1. Add Smart Device"
              "\n2. Control Device\n"
              "\nEnter: ");
       scanf("%d", &choice);
       if(choice == 1){
-            addSmartDevice(choice);
+            addSmartDevice(s, choice, deviceType, one, two, three);
       }
-
-
-      //Devices
-      // printf("\n1. Light bulb"
-      //        "\n2. Motor"
-      //        "\n3. Temp Sensor");
-
+      else if(choice == 2){
+            // run Control Device
+      }
+      else{
+            printf("Invalid Input!!\ntry again");
+      }
 
       return 0;
 }
 
 //Function for Add Smart Device menu
-void addSmartDevice(int choice){
+int addSmartDevice(DeviceInfo *s, int choice, int deviceType, const char *one,
+                   const char *two, const char *three){
       printf("\nDevice Type: "
             "\n1. Light"
             "\n2. Motors"
             "\n3. Sensors\n"
             "\nEnter: ");
-      scanf("%d", &choice);
+      scanf("%d", &deviceType);
 
       // switch for choices
-}
-int connectDevices(DeviceInfo *s, int count){
-      printf("Device Name: \n");
-      scanf("%c", &s->device);
+      switch (deviceType)
+      {
+      case 1:
+            printf("\nName your light: \n");
+            printf("%s 1: ", one);
+            scanf("%c",)
+            break;
+      
+      case 2:
+            printf("\nName your motor: \n");
+            printf("%s 2: ", two);
+      
+      case 3:
+            printf("\nName your sensor: \n");
+            printf("%s 3: ", three);
+
+      default:
+            break;
+      }
 
       return 0;
 }
